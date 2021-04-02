aedadf73e3a5fa338f8c6a29b6472725bd3cf7f1
alexn400 <alexn400@gmail.com>
2021-04-02 17:56:13 +1300

    Add bootstrap

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import App from './App';
 import authConfig from './authConfig';
 import store from './redux/store';
+import './styles/index.scss';
 
 Auth.configure(authConfig);
 
